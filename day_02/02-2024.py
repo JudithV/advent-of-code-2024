8c8115176e4ac8ea1bba9790c0fe2f27bd688bed
Judith Vilella Cantos <judithvc2209@gmail.com>
2024-12-24 15:46:12 +0100

    day 2. day 3 start

--- day_02/02-2024.py
+++ day_02/02-2024.py
@@ -1,32 +1,23 @@
 def count_safe_reports(reports):
     safe = 0
-    report_start = True
     unsafe_diff = False
-    previous_level = 0
-    for report in reports:      
+    for report in reports:   
         sorted_levels = sorted(report)
         sorted_desc_levels = sorted(report, reverse=True)
-        if sorted_levels == report or sorted_desc_levels == report:
-            for level in report:
-                if report_start:
-                    report_start = False
-                else:
-                    if abs(level - previous_level) > 3 or abs(level-previous_level) < 1:
-                        unsafe_diff = True        
-                previous_level = level
+        if sorted_levels == report or sorted_desc_levels == report:   
+            for i in range(len(report)-1):
+                if abs(report[i] - report[i+1]) > 3 or abs(report[i]-report[i+1]) < 1:
+                    unsafe_diff = True 
             if not unsafe_diff:
                 safe += 1
-        report_start = True
-        unsafe_diff = False
+            unsafe_diff = False
     return safe
 
 def levels_from_file(fileName: str):
     reports = []
     input = open(fileName, 'r')
     for line in input:
-        levels_str = line.split()
-        levels = list(map(int, levels_str))
-        reports.append(levels)
+        reports.append(list(map(int, line.split())))
     return reports
 
 if __name__ == '__main__':
